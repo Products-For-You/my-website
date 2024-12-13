541687db3ee649282478bd3363bd4a4fd67b1c20
Products-For-You <raygrafter@gmail.com>
2024-12-13 06:33:43 +0000

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
-  alert("Welcome to My Website!");
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//   alert("Welcome to My Website!");
+// });
 
 document.getElementById("darkModeToggle").addEventListener("click", function() {
   document.body.classList.toggle("dark-mode");
